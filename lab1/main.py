eb4b3f6680357502e58c555302ef624511c68250
Фурдик Никита <nikita.furdik@inside.glowbyte.com>
2022-03-09 22:47:14 +0300

    upd lab1

--- lab1/main.py
+++ lab1/main.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import seaborn as sb
+
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.metrics import silhouette_score, davies_bouldin_score
@@ -12,7 +14,8 @@ from mpl_toolkits.mplot3d import Axes3D
 le, ms = LabelEncoder(), MinMaxScaler()
 
 #categories = ['Vendor', 'Category', 'Item', 'Item Description', 'Price', 'Origin', 'Rating']
-categories = ['product_title', 'ships_from_to', 'grams', 'quality', 'btc_price', 'cost_per_gram', 'cost_per_gram_pure', 'escrow', 'product_link']
+#
+categories = ['product_title', 'ships_from_to', 'quality', 'btc_price', 'cost_per_gram', 'escrow', 'product_link']
 
 def plt_compare(df):
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15,8))
@@ -20,7 +23,7 @@ def plt_compare(df):
     silhouette_scores = []
     elbow_scores = []
     bolduin_scores = []
-    for i in range(2, 6):
+    for i in range(2, 10):
         kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 1000, n_init = 10, random_state = 0)
         kmeans.fit_predict(df)
         elbow_scores.append(kmeans.inertia_)
@@ -29,9 +32,9 @@ def plt_compare(df):
 
     plt.ylabel('WGCS')
     plt.xlabel('кол-во кластеров')
-    ax1.plot(range(2,6), elbow_scores)
-    ax2.plot(range(2,6), silhouette_scores)
-    ax3.plot(range(2,6), bolduin_scores)
+    ax1.plot(range(2,10), elbow_scores)
+    ax2.plot(range(2,10), silhouette_scores)
+    ax3.plot(range(2,10), bolduin_scores)
     ax1.set_title('метод локтя')
     ax2.set_title('метод силуэта')
     ax3.set_title('davis-bolduin')
@@ -51,30 +54,30 @@ def kmeans(df, clusters):
     fig = plt.figure()
     ax = Axes3D(fig)
 
-    kmeans = KMeans(n_clusters = clusters, init = 'k-means++', max_iter = 1000, n_init = 10, random_state = 0)
+    kmeans = KMeans(n_clusters = clusters, init = 'k-means++', max_iter = 100, n_init = 10, random_state = 0)
     labels = kmeans.fit_predict(df)
 
     centroids = kmeans.cluster_centers_
     u_labels = np.unique(labels)
     for i in u_labels:
-        ax.scatter(df.iloc[labels == i , 0], df.iloc[labels == i , 1], label = i)
+        ax.scatter(df.iloc[labels == i , 0], df.iloc[labels == i , 1], df.iloc[labels == i , 2], label = i)
         #axs[0].scatter(df.iloc[labels == i , 0], df.iloc[labels == i , 1], label = i)
         #axs[1,0].scatter(df.iloc[labels == i , 3], df.iloc[labels == i , 4], df.iloc[labels == i , 5], label = i)
-    ax.scatter(centroids[:,0] , centroids[:,1] , s = 10, color = 'black')
+    ax.scatter(centroids[:,0] , centroids[:,1] , centroids[:,2] , s = 10, color = 'black')
     #axs[1,0].scatter(centroids[:,0] , centroids[:,1] , s = 80, color = 'black')
     plt.show()
 
 def dbscan(df):
     fig = plt.figure()
     ax = Axes3D(fig)
-    db = DBSCAN(eps=0.367, min_samples=18).fit(df) # 2 * число параметров
+    db = DBSCAN(eps=0.3320, min_samples=18).fit(df) # 2 * число параметров
     labels = db.fit_predict(df)
     u_labels = np.unique(labels)
     for i in u_labels:
-        ax.scatter(df.iloc[labels == i , 0], df.iloc[labels == i , 1], label = i)
+        ax.scatter(df.iloc[labels == i , 0], df.iloc[labels == i , 1], df.iloc[labels == i , 2], label = i)
         #axs[1].scatter(df.iloc[labels == i , 0], df.iloc[labels == i , 1], label = i)
         #axs[1,1].scatter(df.iloc[labels == i , 3], df.iloc[labels == i , 4], df.iloc[labels == i , 5], label = i)
-
+    print(u_labels)
     #axs[0].set_title('k средних')
     #axs[1].set_title('db scan')
     plt.show()
@@ -82,18 +85,33 @@ def dbscan(df):
 a = input()
 #df = pd.read_csv('Agora.csv', delimiter=',', encoding ='latin-1')
 df = pd.read_csv('dream_market_cocaine_listings.csv', delimiter=',', encoding ='latin-1')
+
 df = df[categories]
+
+
 #df.drop(['Destination', 'Remarks'], axis=1, inplace=True)
 
 for category in categories:
     df[category] = le.fit_transform(df[category])
 
 df = pd.DataFrame(ms.fit_transform(df), columns=categories)
+
+pearsoncorr = df.corr(method='pearson')
+ax = sb.heatmap(pearsoncorr,
+            xticklabels=pearsoncorr.columns,
+            yticklabels=pearsoncorr.columns,
+            cmap='RdBu_r',
+            annot=True,
+            linewidth=0.5)
+
+plt.show()
+print(pearsoncorr)
+
 print(df)
 
 if a == 'k':
     plt_compare(df)
-    kmeans(df, 4)
+    kmeans(df, 7)
 else:
     get_eps(df)
     dbscan(df)
